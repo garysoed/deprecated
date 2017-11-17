e32d308b83efb3634d2191f078c41a84620965ca
Gary Soedarsono <garysoed@gmail.com>
2017-11-17 08:33:58 -0800

    Cleaned up preview to not use gs_ui

--- src/preview/main.ts
+++ src/preview/main.ts
@@ -1,17 +1,13 @@
 import { Jsons } from 'external/gs_tools/src/data';
+import { Injector } from 'external/gs_tools/src/inject';
+import { Persona } from 'external/gs_tools/src/persona';
 import { Templates } from 'external/gs_tools/src/webc';
-
-import { Main } from 'external/gs_ui/src/bootstrap';
-import { DefaultPalettes, Theme } from 'external/gs_ui/src/theming';
 
 import { PreviewView } from '../preview/preview-view';
 
-
-const theme = Theme.newInstance(
-    DefaultPalettes.get('green'),
-    DefaultPalettes.get('orangepeel'));
+[PreviewView].forEach(() => undefined);
 window.addEventListener('load', () => {
-  Main.newInstance().bootstrap(theme, [PreviewView]);
+  Persona.registerAll(Injector.newInstance(), Templates.newInstance());
 });
 
 Jsons.setValue(window, 'gs.Templates', Templates);
