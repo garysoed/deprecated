72555b86dc1ae44350acfe5370b59663ed84dccd
Gary Soedarsono <garysoed@gmail.com>
2017-11-19 01:50:50 -0800

    render defaults

---
+++ src/render/render-default-script.ts
@@ -0,0 +1,8 @@
+import { Main } from 'external/gs_ui/src/bootstrap';
+import { DefaultPalettes, Theme } from 'external/gs_ui/src/theming';
+
+const theme = Theme.newInstance(
+    DefaultPalettes.get('green'),
+    DefaultPalettes.get('orangepeel'));
+
+Main.newInstance({}, window['gs'].Templates.newInstance()).bootstrap(theme)
