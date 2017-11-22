d4b1aa1c24a385b78134e336e1699ca10fedc8c2
Gary Soedarsono <garysoed@gmail.com>
2017-11-21 19:38:47 -0800

    Small changes

--- src/render/showdown-service.ts
+++ src/render/showdown-service.ts
@@ -3,7 +3,9 @@ import { cache } from 'external/gs_tools/src/data';
 export class ShowdownServiceClass {
   @cache()
   private getConverter_(): showdown.Converter {
-    return new showdown.Converter();
+    return new showdown.Converter({
+      tasklists: true,
+    });
   }
 
   render(raw: string): string {
