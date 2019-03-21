2a3a8c59fabf69fc5801dea4d4e4a40645f1767e
Gary Soedarsono <garysoed@gmail.com>
2019-03-20 23:08:35 -0700

    Used the APIs

--- src/view/projectlist/project-list-view.ts
+++ src/view/projectlist/project-list-view.ts
@@ -1,9 +1,8 @@
 import { instanceSourceId } from 'grapevine/export/component';
 import { ElementWithTagType, StringType } from 'gs-types/export';
-import { _p, _v, IconWithText, TextIconButton, TextInput, ThemedCustomElementCtrl } from 'mask/export';
-import { identity } from 'nabu/export/util';
-import { attributeIn, element, onDom } from 'persona/export/input';
-import { attributeOut, caller } from 'persona/export/output';
+import { $textIconButton, $textInput, _p, _v, IconWithText, TextIconButton, TextInput, ThemedCustomElementCtrl } from 'mask/export';
+import { element } from 'persona/export/input';
+import { api } from 'persona/export/main';
 import { Observable } from 'rxjs';
 import { switchMap, tap, withLatestFrom } from 'rxjs/operators';
 import { $projectCollection, ProjectCollection } from '../../datamodel/project-collection';
@@ -13,14 +12,8 @@ import projectListViewTemplate from './project-list-view.html';
 export const DEFAULT_PROJECT_NAME = 'New Project';
 
 export const $ = {
-  addButton: element('addButton', ElementWithTagType('mk-text-icon-button'), {
-    onClick: onDom('click'),
-  }),
-  addProjectName: element('addProjectName', ElementWithTagType('mk-text-input'), {
-    clearFn: caller<[void]>('clear'),
-    initValue: attributeOut('init-value', identity()),
-    value: attributeIn('value', identity(), StringType),
-  }),
+  addButton: element('addButton', ElementWithTagType('mk-text-icon-button'), api($textIconButton)),
+  addProjectName: element('addProjectName', ElementWithTagType('mk-text-input'), api($textInput)),
 };
 
 const $initProjectName = instanceSourceId('initProjectName', StringType);
@@ -39,11 +32,11 @@ _v.builder.source($initProjectName, DEFAULT_PROJECT_NAME);
 export class ProjectListView extends ThemedCustomElementCtrl {
   @_p.render($.addProjectName._.clearFn)
   onAddButtonClick_(
-      @_p.input($.addButton._.onClick) clickEventObs: Observable<Event>,
+      @_p.input($.addButton._.actionEvent) actionEventObs: Observable<Event>,
       @_p.input($.addProjectName._.value) projectNameInputValueObs: Observable<string>,
       @_v.vineIn($projectCollection) projectCollectionObs: Observable<ProjectCollection>,
   ): Observable<unknown> {
-    return clickEventObs
+    return actionEventObs
         .pipe(
             withLatestFrom(projectCollectionObs),
             switchMap(([, projectCollection]) => projectCollection.newProject()),
